8c4c0cba1f1c01be4466befcc454d9e9d37e658f
Saulo Henrique Rodrigues <saulohrodrigues@gmail.com>
2026-01-26 13:43:04 -0300

    build: configurações iniciais;

---
+++ src/main/java/com/db/desafio/naruto01/Naruto01Application.java
@@ -0,0 +1,13 @@
+package com.db.desafio.naruto01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Naruto01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Naruto01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/desafio/naruto01/Naruto01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.desafio.naruto01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Naruto01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
